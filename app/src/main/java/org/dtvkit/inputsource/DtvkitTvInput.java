ede9b4a771222c211e6d5741dce0698c9c29638c
ming.an <ming.an@amlogic.com>
2024-03-05 10:44:34 +0800

    atf: enter and exit home, channel not play [1/1]
    PD#SWPL-159365
    
    Problem:
    small probability of not get tuner resource due to timing
    
    Solution:
    add protection mechanism to obtain tuner resources
    
    Verify:
    T5M
    
    Change-Id: If0d7b0457eac5fffbc28e3a16d92b3eec5ec5da2
    Signed-off-by: ming.an <ming.an@amlogic.com>

--- app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
@@ -7243,6 +7243,11 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                     if (null != mMainTuner) {
                         mMainTuner.setSurfaceToNative(surface);
                         mBackgroundTuner.setSurfaceToNative(surface);//set surface to background tuner for prepare other using
+                    } else {
+                        Log.d(TAG, "setLiveTunerSurface error main tuner is null need new one");
+                        initLiveTuner(outService, null, false);
+                        mMainTuner.setSurfaceToNative(surface);
+                        mBackgroundTuner.setSurfaceToNative(surface);
                     }
                 }
             }
